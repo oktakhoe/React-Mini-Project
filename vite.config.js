95ea471f8a823408b954e9dc5738571c15a479d2
Putri Oktavianti <oktakhoe@gmail.com>
2023-12-31 10:03:06 +0700

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
->>>>>>> 4c343b9 (edited MD)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> 4c343b9 (edited MD)
